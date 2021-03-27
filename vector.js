8de8252440fd9174b0aeec6d418d35f939d0280b
Karey Higuera <karey.higuera@gmail.com>
2021-03-27 08:46:26 -0500

    Add vector utility file with add and distance

---
+++ vector.js
@@ -0,0 +1,16 @@
+const Vector = {
+  add: (...vectors) => vectors.reduce((sum, vector) => {
+    if(sum.length !== vector.length){
+      throw new Error('Cannot add vectors of different lengths')
+    }
+    return sum.map((item, index) => item + vector[index])
+  }),
+  distance: (vector1, vector2) => {
+    if(vector1.length !== vector2.length){
+      throw new Error('Cannot find distance between vectors of different lengths')
+    }
+    return Math.sqrt(vector1.reduce((sum, element, index) => sum + (element - vector2[index]) ** 2))
+  }
+}
+
+console.log(Vector.distance([1,1], [0,0]))
